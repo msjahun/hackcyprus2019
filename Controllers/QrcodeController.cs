38b46dbe79a731f50748cf230fa792ac6f1e6dab
Musa Jahun <msjahun@live.com>
2019-07-21 13:58:28 +0200

    added qr scanner page and web page view for displaying the qr code

---
+++ Controllers/QrcodeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parking_Alert.Controllers
+{
+    // QrcodeController
+
+    public class QrcodeController : Controller
+    {
+        public IActionResult QrCodeScan()
+        {
+            return View();
+        }
+
+       
+    }
+
+}
